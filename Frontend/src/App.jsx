b5605408fb55c79659e1b61980ee2ba4c002bf21
saishshaikh <saishshaikh2323@gmail.com>
2026-02-22 21:44:29 +0530

    Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import Home from './Pages/Login'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route  path='/Home' element={<Home/>}/>
+        <Route  path='/Login' element={<Login/>}/>
       </Routes>
     </div>
   )
