5d0afb29b25c5c26d0b2120a45fbdb9324e4b9e5
saishshaikh <saishshaikh2323@gmail.com>
2026-03-08 13:57:33 +0530

    Google Auth Working in Console

--- Backend/index.js
+++ Backend/index.js
@@ -4,17 +4,23 @@ dotenv.config();
 import { DBconnect } from "./Config/db.js";
 import Authrouter from "./Routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json());          
-app.use(cookieParser());          
-app.use("/api/auth", Authrouter); 
-app.use(cors({
-  origin:"http://localhost:5173",
-  Credential : true
-}))
+app.use(express.json());
+app.use(cookieParser());
+
+// ✅ CORS must come BEFORE routes
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
+
+// routes
+app.use("/api/auth", Authrouter);
 
 const port = process.env.PORT || 8000;
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
-
-
+export const serverUrl="http://localhost:8000"
 const App = () => {
   return (
     <div>
